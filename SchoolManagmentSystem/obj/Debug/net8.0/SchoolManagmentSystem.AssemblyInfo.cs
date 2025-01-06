a374559d8fa6860f4628b1c89abcd279ebbb0b74
Darko Dimitrov <darkodimitrov10@yahoo.com>
2025-01-07 00:24:25 +0100

    Kubernetes

--- SchoolManagmentSystem/obj/Debug/net8.0/SchoolManagmentSystem.AssemblyInfo.cs
+++ SchoolManagmentSystem/obj/Debug/net8.0/SchoolManagmentSystem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagmentSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b76fefde6c13dec1866fc114044b5a57e55ea1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99fc2edce94bb8c206b302dac95d32429e31f26a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagmentSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagmentSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
